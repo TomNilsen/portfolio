a1ad3b91ac6e1dacaa2834ddb6ee1fcc271c2445
TomNilsen <137330265+TomNilsen@users.noreply.github.com>
2023-06-22 14:57:43 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,42 +1,6 @@
+import { projects } from "./projects.js";
+
 const main = document.querySelector("main");
-
-const project1 = {
-  name: "Kreativ programmering",
-  img: "/images/Clocks.png",
-  alt: "Clocks",
-  desc: "Under kursen Kreativ programmering arbetade jag med det externa Javascript biblioteket p5.js. P5 låter programmeraren skapa unika visuella animationer, infoga musik och ljudeffekter samt skapa enkla spel med Javascript kod. Animationen föreställande flera klockor som börjar ticka när användaren klickar på skärmen. Animationen och formerna är helt skapade i kod.",
-  demo: "https://tomnilsen.github.io/clocks/",
-  github: "https://github.com/TomNilsen/clocks",
-};
-
-const project2 = {
-  name: "Praktik för Södertörns högskola externa relationer",
-  img: "/images/SHUB.png",
-  alt: "SHUB",
-  desc: "Under praktik perioden arbetade jag med att skapa en prototyp av ett tidigare examensprojekt som två tidigare elever skapat. Examensarbetet gick ut på att skapa en databas där studenter kunde komma i kontakt med uppdragsgivare för praktik och andra projekt. Examensarbetet var skapat i Adobe XD och det var min uppgift att skapa en prototyp i HTML, CSS, Javascript, PHP, SQL och jQuery kod. Jag har alltså inte gjort några av dem grafiska designvalen utan endast översatt Adobe XD projektet till kod.",
-  demo: "",
-  github: "",
-};
-
-const project3 = {
-  name: "Examensarbete för It, medier & design",
-  img: "/images/HBTQsocial.png",
-  alt: "HBTQ-social",
-  desc: "Som sista moment på utbildningen It, medier och design arbetade jag på ett enskilt examensprojekt under 10 veckor. Under perioden skapade jag en social medieplattform för HBTQIA+-personer där användarna kan skapa och delta på varandras evenemang. På hemsidan kan användaren skapa ett konto som närsomhelst går att ändra eller radera, samt skapa evenemang som närsomhelst kan ändras eller raderas. Hemsidan är uppbyggd med HTML, CSS, Javascript, jQuery, PHP och SQL kod, de grafiska elementen är skapade i adobe Illustrator. Idén till projektet och alla dess delar är helt skapade av mig, jag har även utfört användartester på personer från relevant målgrupp för att förbättra hemsidan.",
-  demo: "",
-  github: "",
-};
-
-const project4 = {
-  name: "Dynamiska webbaplikationer CSS upgift",
-  img: "/images/Surf.png",
-  alt: "LOREM IPSUM SURF CO",
-  desc: "Under kursen Dynamiska webbapplikationer fick vi skapa CSS koden för en hemsida med färdig HTML kod. Hemsidan är skapad för ett fiktivt företag och innehåller ett formulär där användaren kan skriva upp sig för nyhetsbrev och en tabell med olika lektioner och priser. Jag använde en CSS-reset fil och kunde därför helt välja stil, färger och bakgrund. Hemsidan är även anpassad för mobil och surfplatta.",
-  demo: "https://tomnilsen.github.io/surf/",
-  github: "https://github.com/TomNilsen/surf",
-};
-
-const projects = [project4, project3, project2, project1];
 
 for (let i = 0; i < projects.length; i++) {
   createCard(projects[i]);
